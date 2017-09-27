7108adb57f5efd9a6b5e6c383e4c6357927f8d37
Alexander Hewitt <94xander@gmail.com>
2017-09-27 16:46:43 +0100

    quackerInital commit

---
+++ Hover.js
@@ -0,0 +1,19 @@
+/*var main = (function() {
+ 
+function hover('.main') {
+    element.setAttribute('src', 'anaphitelogohover.png');
+    }
+function unhover(element) {
+    element.setAttribute('src', 'anaphitelogoblack.png');
+}
+ 
+})();
+function onHover()
+{
+    $("#logoNav").attr('src', 'anaphitelogohover.png?');
+}
+
+function offHover()
+{
+    $("#logoNav").attr('src', 'anaphitelogoblack.png?');
+}*/
